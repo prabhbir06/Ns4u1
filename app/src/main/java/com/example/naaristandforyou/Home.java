e52f6fac5ef296f419ec459dd9b4b682caa5f874
karnvi897 <64742909+karnvi897@users.noreply.github.com>
2020-05-05 16:03:42 +0530

    hi

--- app/src/main/java/com/example/naaristandforyou/Home.java
+++ app/src/main/java/com/example/naaristandforyou/Home.java
@@ -30,7 +30,7 @@ public class Home extends AppCompatActivity {
         space.addSpaceItem(new SpaceItem("Home", R.drawable.ic_home_black_24dp));
         space.addSpaceItem(new SpaceItem("Profile", R.drawable.ic_person_black_24dp));
         space.addSpaceItem(new SpaceItem("Share", R.drawable.ic_home_black_24dp));
-        space.addSpaceItem(new SpaceItem("Settings", R.drawable.ic_person_black_24dp));
+        space.addSpaceItem(new SpaceItem("Settings", R.drawable.border));
 
         space.setSpaceOnClickListener(new SpaceOnClickListener() {
             @Override
